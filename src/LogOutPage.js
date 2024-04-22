b00825fc6e49098894442106cb72d891b088f275
Naveen-369 <nk9363690@gmail.com>
2024-04-23 00:00:47 +0530

    Added the Loging page succesfully

--- src/LogOutPage.js
+++ src/LogOutPage.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import './LoginCss.css';
 
+
 function LogOutPage() {
+  const handleLoginClick = () => {
+    const username = document.getElementById('UserName').value;
+    const password = document.getElementById('Password').value;
+    if (username === 'Naveen Kumar' && password === '6369629703') {
+      window.location.href = "/home";
+      console.log('Login Successful');
+    } else {
+      window.location.reload();
+    }
+  };
   return (
     <div>
       <img src='LogInWp.jpg' className="background-image" alt='Wallpaper'/>
       <div className="centered-box">
         <section id="inputter">
           <label htmlFor="UserName">User Name</label>
-          <input type='text' id='UserName' placeholder='Enter your User ID or Email - Address' /><br />
+          <input type='text' id='UserName' placeholder='User ID or Email - Address' /><br />
           <label htmlFor="Password">Password</label>
           <input type='password' id='Password' placeholder='Enter your Password' />
-          <button type="button">Login</button>
+          <button type="button" onClick={handleLoginClick}>Login</button>
         </section>
       </div>
     </div>
